5f27c0cbf68c5c43beac0934856a6f8c03570b51
Kefrost <50832663+Kefrost@users.noreply.github.com>
2019-05-25 19:35:05 +0300

    Create 04_Print_and_sum.cs

---
+++ Basic Syntax, Conditional Statements and Loops/04_Print and sum/04_Print_and_sum.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
